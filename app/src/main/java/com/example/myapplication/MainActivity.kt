5a48a90d7bb1538dad9a51ff8b80dfed0b47559f
Adham Faraitodi <109128115+adhamfaraitodi@users.noreply.github.com>
2023-08-18 19:22:22 +0700

    add new activity

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.EditText
 import android.widget.Toast
 
+const val PESAN = "com.example.myapplication.PESAN"
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,5 +18,8 @@ class MainActivity : AppCompatActivity() {
         val pesan = editText.text.toString()
         val toast = Toast.makeText(applicationContext, pesan, Toast.LENGTH_SHORT)
         toast.show()
+        val intent = Intent(this, DisplayMessageActivity::class.java)
+        intent.putExtra(PESAN, pesan)
+        startActivity(intent)
     }
 }
